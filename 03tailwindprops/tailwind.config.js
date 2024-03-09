bd405ac6682734fbe23198d3c12bb6d649b74320
SahilPatil7777 <sahilpatil32267@gmail.com>
2024-03-09 10:48:28 +0530

    Tailwind and Props in react

---
+++ 03tailwindprops/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // it search content in index and src
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
